d0dd27f5667396cfdfa29ea278f6f846ccdfb9ae
Harry <Harry_emmett@outlook.com>
2021-12-13 20:45:04 +0000

    HTP2-31 #comment added frontend create functionality

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,1 +1,25 @@
 "use strict";
+
+//CREATE FUNCTIONALITY
+document.querySelector("#createMovie").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const createMovieForm = this;
+
+    const movieData = {
+        movieName: createMovieForm.movieName.value,
+        genre: createMovieForm.genre.value,
+        yearReleased: createMovieForm.yearReleased.value,
+        availableOn: createMovieForm.availableOn.value,
+    };
+
+    axios
+        .post("http://localhost:8080/createMovie", movieData)
+        .then(response => {
+            console.log(response);
+            createMovieForm.reset();
+            createMovieForm.name.focus();
+        })
+        .catch(error => console.log(error))
+
+});
