e47a13e195b53b1946f43e68892e2cdfcc50b551
Harry <Harry_emmett@outlook.com>
2021-12-13 22:25:52 +0000

    HTP2-32 #comment added read all functionality

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const getAllMovies = document.querySelector("#getAllMovies");
+
 //CREATE FUNCTIONALITY
 document.querySelector("#createMovie").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -16,10 +18,69 @@ document.querySelector("#createMovie").addEventListener("submit", function(event
     axios
         .post("http://localhost:8080/createMovie", movieData)
         .then(response => {
+            getAllMovies.innerHTML = "";
+            getMovies();
             console.log(response);
             createMovieForm.reset();
-            createMovieForm.name.focus();
         })
         .catch(error => console.log(error))
 
 });
+
+//READ FUNCTIONALITY
+const getMovies = () => {
+    axios
+        .get("http://localhost:8080/getAll")
+        .then(response => {
+            console.log(response);
+            const moviesList = response.data;
+            // getAllMovies.innerHTML = "";
+            for(let movie of moviesList) {
+
+                const userContainer = document.createElement("div");
+                // movieColumn.classList.add("allMovies");
+                // userContainer.classList.add("allMovies");
+
+                // const movieCard = document.createElement("div");
+                // movieCard.style = `background-color: red`;
+                // movieCard.classList.add("card");
+
+                // const movieBody = document.createElement("div");
+                // movieBody.classList.add("card-body");
+
+                const movieName = document.createElement("h3");
+                movieName.classList.add("card-title");
+                movieName.innerText = movie.movieName;
+                userContainer.appendChild(movieName);
+
+                const genre = document.createElement("p");
+                genre.classList.add("card-text");
+                genre.innerText = `Genre: ${movie.genre}`;
+                userContainer.appendChild(genre);
+                
+                const yearReleased = document.createElement("p");
+                yearReleased.classList.add("card-text");
+                yearReleased.innerText = `Released: ${movie.yearReleased}`;
+                userContainer.appendChild(yearReleased);
+
+                const availableOn = document.createElement("p");
+                availableOn.classList.add("card-text");
+                availableOn.innerText = `Available on: ${movie.availableOn}`;
+                userContainer.appendChild(availableOn);
+
+                // // movieCard.appendChild(movieBody);
+                // userContainer.appendChild(movieCard);
+
+                console.log(userContainer);
+
+                getAllMovies.appendChild(userContainer);
+        
+
+            
+           }
+    })
+    .catch(error => console.log(error))
+
+}
+
+getMovies();
